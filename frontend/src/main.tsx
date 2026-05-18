f6efe0e3f965104efb87a42be856a698301a9cfb
brotherman487 <CJBIGGINS2300@GMAIL.COM>
2026-05-18 14:37:15 -0500

    chore: remove frontend app

--- frontend/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-
-// Apply saved theme before first render to avoid flash
-const savedTheme = localStorage.getItem('edon_theme') ?? 'dark';
-document.documentElement.classList.add(savedTheme);
-
-createRoot(document.getElementById("root")!).render(<App />);
